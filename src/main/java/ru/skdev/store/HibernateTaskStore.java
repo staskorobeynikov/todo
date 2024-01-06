c8e7c8fbc16cc19f44add014bf413729bcc2bcbf
staskorobejnikov <stas.korobeinikov@mail.ru>
2024-01-06 19:26:16 +0400

    add implementation TaskStore

---
+++ src/main/java/ru/skdev/store/HibernateTaskStore.java
@@ -0,0 +1,198 @@
+package ru.skdev.store;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+import ru.skdev.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@AllArgsConstructor
+@Repository
+public class HibernateTaskStore implements TaskStore {
+    private final SessionFactory factory;
+
+    @Override
+    public Optional<Task> create(Task task) {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(task);
+            transaction.commit();
+            return Optional.of(task);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Task> findAll() {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        List<Task> result = new ArrayList<>();
+        try {
+            transaction = session.beginTransaction();
+            result = session.createQuery(
+                    """
+                            FROM Task t
+                            ORDER BY t.id
+                            """,
+                    Task.class
+            ).getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Task> findUndoneTasks() {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        List<Task> result = new ArrayList<>();
+        try {
+            transaction = session.beginTransaction();
+            result = session.createQuery(
+                    """
+                            FROM Task t
+                            WHERE t.done = false
+                            ORDER BY t.id
+                            """,
+                    Task.class
+            ).getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Task> findDoneTasks() {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        List<Task> result = new ArrayList<>();
+        try {
+            transaction = session.beginTransaction();
+            result = session.createQuery("""
+                            FROM Task t
+                            WHERE t.done = true
+                            ORDER BY t.id
+                            """,
+                    Task.class
+            ).getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean update(int id, Task task) {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            int result = session
+                    .createQuery(
+                            """
+                                    UPDATE Task t
+                                    SET t.title = :title,
+                                        t.description = :description
+                                    WHERE t.id = :id
+                                    """
+                    ).setParameter("title", task.getTitle())
+                    .setParameter("description", task.getDescription())
+                    .setParameter("id", id)
+                    .executeUpdate();
+            transaction.commit();
+            return result > 0;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            int result = session.createQuery(
+                            """
+                                    DELETE FROM Task t
+                                    WHERE t.id = :id
+                                    """,
+                            Task.class
+                    ).setParameter("id", id)
+                    .executeUpdate();
+            transaction.commit();
+            return result > 0;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateDone(int id) {
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            int result = session.createQuery(
+                            """
+                                    UPDATE Task t
+                                    SET t.done = true
+                                    WHERE t.id = :id
+                                    """,
+                            Task.class
+                    ).setParameter("id", id)
+                    .executeUpdate();
+            transaction.commit();
+            return result > 0;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return false;
+    }
+}
---
+++ src/main/java/ru/skdev/store/TaskStore.java
@@ -0,0 +1,22 @@
+package ru.skdev.store;
+
+import ru.skdev.model.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskStore {
+    Optional<Task> create(Task task);
+
+    List<Task> findAll();
+
+    List<Task> findUndoneTasks();
+
+    List<Task> findDoneTasks();
+
+    boolean update(int id, Task task);
+
+    boolean delete(int id);
+
+    boolean updateDone(int id);
+}
